e63b23c66918f00b5f4708b5b9e973057c457e5c
Manutronics <manutronics@gmail.com>
2024-02-22 14:59:29 -0300

    added LIDAR alike sensor

--- car.js
+++ car.js
@@ -12,12 +12,14 @@ class Car{
         this.friction = 0.05;
         this.angle = 0;
 
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
     update()
     {
         this.#move();
+        this.sensor.update();
     }
 
     #move()
@@ -85,5 +87,6 @@ class Car{
         );
         ctx.fill();
         ctx.restore();
+        this.sensor.draw(ctx);
     }
 }
